31920ec6cbfa2dc67c04c2830449014f88b7d7a1
luismanueldiaz29 <luismaprograma@gmail.com>
2022-10-18 18:40:18 -0500

    Se creo las rutas para la navegación

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+  
 function App() {
-  return (
-    <div className="App">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum dicta hic, a iusto ipsa exercitationem, saepe cumque doloremque mollitia iure cum ad, debitis aliquid quae consequuntur deleniti aliquam accusamus atque.
-    </div>
-  );
-}
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+
+            <Route path="/about" element={<About />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/" element={<Home />} />
+
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
+  
+  function Home() {
+    return <h2>Home</h2>;
+  }
+  
+  function About() {
+    return <h2>About</h2>;
+  }
+  
+  function Users() {
+    return <h2>Users</h2>;
+  }
+  
 
 export default App;
